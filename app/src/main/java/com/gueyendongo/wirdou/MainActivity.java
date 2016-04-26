ac8300b32fd0571ad25851874182b43f77306705
Bocar Gueye <bgueye@gmail.com>
2016-04-26 22:59:40 +0200

    Deuxieme commit

--- app/src/main/java/com/gueyendongo/wirdou/MainActivity.java
+++ app/src/main/java/com/gueyendongo/wirdou/MainActivity.java
@@ -2,38 +2,35 @@ package com.gueyendongo.wirdou;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
-import android.widget.TimePicker;
-
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar =   (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 //Get a new instance of Calendar
         final Calendar c= Calendar.getInstance();
-        int hourOfDay = c.get(c.HOUR_OF_DAY); //Current Hour
-        int minute = c.get(c.MINUTE); //Current Minute
-        int second = c.get(c.SECOND); //Current Second
+        int jour = c.get(c.DAY_OF_WEEK); //Jour de la semaine
+        int day = c.get(c.DAY_OF_MONTH); //jour
+        int mois = c.get(c.MONTH); //Mois
+        int annee = c.get(c.YEAR); //année
 
         //Get the widgets reference from XML layout
         final TextView tv = (TextView) findViewById(R.id.tv);
-        TimePicker tp = (TimePicker) findViewById(R.id.tp);
+
 
         //Display the TimePicker initial time
-        tv.setText("Initial Time\nH:M:S | " + hourOfDay + ":" + minute + ":" + second);
+        tv.setText("Date du jour\n" + jour + ", " + day + " " + mois + " " + annee );
 
-        //Set a TimeChangedListener for TimePicker widget
-        tp.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                //Display the new time to app interface
-                tv.setText("Time changed\nH:M | "+hourOfDay + ":" + minute);
-            }
-        });
     }
 }
